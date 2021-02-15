d9519185c805e04c69b6c46b328e25430f23f4f1
kva-devops <kva.devops@gmail.com>
2021-02-15 16:35:19 +0000

    edit 1

--- webServer.tf
+++ webServer.tf
@@ -11,16 +11,7 @@ resource "aws_instance" "my_webserver" {
 	ami = "ami-0a6dc7529cd559185"
 	instance_type = "t2.micro"
 	vpc_security_group_ids = [aws_security_group.my_webserver.id]
-	user_data = <<EOF
-#!/bin/bash
-yum -y update
-yum -y install httpd
-myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-echo "<h2> My ip is: $myip</h2><br>Build by Terraform." > /var/www/html/index.html
-sudo service httpd start
-chkconfig httpd on		
-EOF
-
+	user_data = file("userData.sh")
 }
 
 resource "aws_security_group" "my_webserver" {
